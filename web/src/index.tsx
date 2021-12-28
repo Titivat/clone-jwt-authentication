408ac9b402d6a69ac006f493115ff835f10497db
titivat <titivatmo@gmail.com>
2021-12-28 16:16:55 +0700

    init react app

---
+++ web/src/index.tsx
@@ -0,0 +1,19 @@
+import { render } from "react-dom";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+} from "@apollo/react-hooks";
+import App from "./App";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql",
+	cache: new InMemoryCache(),
+});
+
+render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
+);
